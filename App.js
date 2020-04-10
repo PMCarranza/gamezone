459485d62412727654283368c6d4c0bc3c67bce3
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-04-10 12:40:17 -0700

    checking link between desktop and github

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './screens/home'
+import Navigator from './routes/homeStack';
+
+export default function App() {
+  return (
+    <Navigator />
+  );
+}
+
